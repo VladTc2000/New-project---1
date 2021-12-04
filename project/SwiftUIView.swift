d535ffbd87e7693fbbac00ef2e54af5c7eb0bc06
Vlad Tcypilev <vladislavcypilev1@mail.ru>
2021-12-04 21:58:46 +0300

    1_group
    stereo/new_brodcast
    
    1+2 group

---
+++ project/SwiftUIView.swift
@@ -0,0 +1,44 @@
+//
+//  SwiftUIView.swift
+//  project
+//
+//  Created by Пользователь on 04.12.2021.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    @State private var showDetail=false
+    var body: some View {
+    
+        HStack {
+            Text("stereo")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Button(action:  {self.showDetail.toggle()}) {
+                Spacer()
+                Text("Начать новый эфир")
+                    .padding()
+                    .frame(height:35)
+                    .background(Color.orange)
+                    .foregroundColor(.white)
+                    .clipShape(RoundedRectangle(cornerRadius:40))
+            }
+        }
+        .padding([.top, .leading, .trailing])
+        .offset( y: -380)
+       
+            
+                }
+      
+
+}
+   
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+    
+}
+    
+
---
+++ project/SwiftUIView2.swift
@@ -0,0 +1,26 @@
+//
+//  SwiftUIView2.swift
+//  project
+//
+//  Created by Пользователь on 04.12.2021.
+//
+
+import SwiftUI
+
+struct SwiftUIView2: View {
+    var body: some View {
+        
+        ZStack{
+            SwiftUIView()
+            ContentView()
+           
+            
+    }
+}
+
+struct SwiftUIView2_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView2()
+    }
+}
+}
